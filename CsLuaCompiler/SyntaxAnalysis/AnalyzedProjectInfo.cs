0432b9cdee7b3015b5ea9127c60ca08a87217b4f
Pilus <pilus@pilus.info>
2015-07-01 23:55:06 +0200

    Population of references in the analyzed project info.

--- CsLuaCompiler/SyntaxAnalysis/AnalyzedProjectInfo.cs
+++ CsLuaCompiler/SyntaxAnalysis/AnalyzedProjectInfo.cs
@@ -1,12 +1,21 @@
 ﻿
 namespace CsLuaCompiler.SyntaxAnalysis
 {
-    using CsLuaCompiler.SolutionAnalysis;
     using System.Collections.Generic;
+    using System.Linq;
+    using ProjectAnalysis;
 
     class AnalyzedProjectInfo
     {
         public ProjectInfo Info { get; set; }
         public Dictionary<string, NameSpace> Namespaces { get; set; }
+        public List<AnalyzedProjectInfo> Referenders { get; private set; }
+
+        public void PopulateReferences(List<AnalyzedProjectInfo> projects)
+        {
+            this.Referenders = projects
+                .Where(project => project.Info.ReferencedProjects.Any(reff => reff.Equals(this.Info.Name)))
+                .ToList();
+        }
     }
 }
