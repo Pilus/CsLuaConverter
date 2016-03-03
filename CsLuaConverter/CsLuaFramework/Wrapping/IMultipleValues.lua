37d0b7e9648fa1d9632396cb9c1ae89125ba70a6
Pilus <pilus@pilus.info>
2016-03-03 11:25:59 +0100

    Lua class and interface for MultipleValues

---
+++ CsLuaConverter/CsLuaFramework/Wrapping/IMultipleValues.lua
@@ -0,0 +1,4 @@
+CsLuaFramework.Wrapping.IMultipleValues = _M.NE({['#'] = function(interactionElement, generics, staticValues)
+    local typeObject = System.Type('IMultipleValues','CsLuaFramework.Wrapping', nil, 0, generics, implements, interactionElement, 'Interface');
+    return 'Interface', typeObject, nil, nil, nil;
+end})
---
+++ CsLuaConverter/CsLuaFramework/Wrapping/MultipleValues.lua
@@ -0,0 +1,41 @@
+CsLuaFramework.Wrapping.MultipleValues = _M.NE({['#'] = function(interactionElement, generics, staticValues)
+    local baseTypeObject, members = System.Object.__meta(staticValues);
+    local implements = {
+        CsLuaFramework.Wrapping.IMultipleValues[generics].__typeof,
+    };
+    local typeObject = System.Type('MultipleValues','CsLuaFramework.Wrapping',baseTypeObject,#(generics),generics,implements,interactionElement);
+    
+    for i=1,#(generics) do
+        _M.IM(members,'Item'..i,{
+            level = typeObject.Level,
+            memberType = 'Property',
+            scope = 'Public',
+            types = {generics[i]},
+            get = function(element)
+                return element[typeObject.level]["Item"..i];
+            end,
+        });
+    end
+    
+    local constructors = {
+        {
+            types = generics,
+            func = function(element, ...)
+                for i = 1, #(generics) do
+                    element[typeObject.level]["Item"..i] = select(i, ...);
+                end
+            end,
+        },
+    };
+    
+    local objectGenerator = function() 
+        return {
+            [1] = {},
+            [2] = {}, 
+            ["type"] = typeObject,
+            __metaType = _M.MetaTypes.ClassObject,
+        }; 
+    end
+    
+    return "Class", typeObject, members, constructors, objectGenerator, implements, nil;
+end})
