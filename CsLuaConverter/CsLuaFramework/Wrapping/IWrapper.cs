db3c5d54e7182bd696205d712d3de0ad2410340d
Pilus <pilus@pilus.info>
2016-03-04 10:55:17 +0100

    Added type translator to the IWrapper interface.

--- CsLuaConverter/CsLuaFramework/Wrapping/IWrapper.cs
+++ CsLuaConverter/CsLuaFramework/Wrapping/IWrapper.cs
@@ -1,10 +1,14 @@
 ﻿namespace CsLuaFramework.Wrapping
 {
+    using System;
     using Lua;
+    
     public interface IWrapper
     {
-        NativeLuaTable Unwrap<T>(string globalVarName) where T : class;
+        NativeLuaTable Unwrap<T>(T obj) where T : class;
         T Wrap<T>(string globalVarName) where T : class;
+        T Wrap<T>(string globalVarName, Func<NativeLuaTable, Type?> typeTranslator) where T : class;
         T Wrap<T>(NativeLuaTable luaTable) where T : class;
+        T Wrap<T>(NativeLuaTable luaTable, Func<NativeLuaTable, Type?> typeTranslator) where T : class;
     }
 }
