949de88fec2a4b44148aaa79e24f26cc617b8f1d
pilus <pilus@pilus.info>
2017-03-31 18:31:41 +0200

    Progress on linq

---
+++ CsLuaConverter/CsLuaConverter/Lua/System/Linq.lua
@@ -0,0 +1,24 @@
+﻿
+
+local NoElements = function()
+    _M.Throw(System.InvalidOperationException._C_0_8736("Sequence contains no elements"));
+end
+
+local NoMatch = function()
+    _M.Throw(System.InvalidOperationException._C_0_8736("Sequence contains no matching element"));
+end
+
+
+_M.RE("System.Collections.IEnumerable", 0, function()
+    return {
+        { -- T OfType<T>()
+            name = "OfType",
+            numMethodGenerics = 1,
+            generics = _M.MG({['TResult'] = 1});
+            signatureHash = 0,
+            func = function(source, methodGenericsMapping, methodGenerics)
+                
+            end,
+        },
+    }
+end);
